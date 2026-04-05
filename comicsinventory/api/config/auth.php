cdbddfd018f63db434cb34c2ade4b727fce95a02
Gabriel Vieira <gabrielvieira840@gmail.com>
2026-04-05 07:36:26 -0300

    add

--- comicsinventory/api/config/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/auth.php
-return [
-    'secret'  => $_ENV['JWT_SECRET']  ?? 'changeme',
-    'issuer'  => $_ENV['JWT_ISSUER']  ?? 'comicsinventory-api',
-    'ttl'     => (int)($_ENV['JWT_TTL'] ?? 1800),
-    'api_key' => $_ENV['API_KEY']     ?? '',   // << ESSENCIAL
-];
