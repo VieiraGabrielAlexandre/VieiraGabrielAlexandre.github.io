a659feeb8fec71118c4a720032ba9a2d6272473c
Gabriel Vieira <gabrielvieira840@gmail.com>
2026-04-05 07:50:07 -0300

    add

---
+++ comicsinventory/assets/js/config.example.js
@@ -0,0 +1,5 @@
+// Renomeie este arquivo para config.js e ajuste a BASE da sua API
+window.CONFIG = {
+    // Ex.: "https://jo4tul2r6g477hsmq3l63j52by0tufeb.lambda-url.sa-east-1.on.aws"  ou  "https://jo4tul2r6g477hsmq3l63j52by0tufeb.lambda-url.sa-east-1.on.aws/comicsinventory/api"
+    API_BASE: "https://jo4tul2r6g477hsmq3l63j52by0tufeb.lambda-url.sa-east-1.on.aws",
+};
---
+++ comicsinventory/assets/js/config.js
@@ -0,0 +1,4 @@
+// Renomeie este arquivo para config.js e ajuste a BASE da sua API
+window.CONFIG = {
+    API_BASE: "https://jo4tul2r6g477hsmq3l63j52by0tufeb.lambda-url.sa-east-1.on.aws",
+};
